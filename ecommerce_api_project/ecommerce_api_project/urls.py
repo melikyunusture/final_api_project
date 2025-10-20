ce02f0647b330435841422c2a95d438e51792251
Melik Yunus <melikact34@gmail.com>
2025-10-20 03:33:21 +0300

    final commit including Crud operations and readme.md file

--- ecommerce_api_project/ecommerce_api_project/urls.py
+++ ecommerce_api_project/ecommerce_api_project/urls.py
@@ -13,7 +13,9 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-""" 
+"""
+
+'''
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -30,3 +32,20 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    '''
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # JWT Authentication
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    
+    # Apps
+    path('api/users/', include('users.urls')),
+    path('api/', include('products.urls')),
+]
--- ecommerce_api_project/users/urls.py
+++ ecommerce_api_project/users/urls.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-
+'''
 # Create your tests here.
 from django.urls import path
 from . import views
@@ -10,4 +10,20 @@ urlpatterns = [
     path('profile/', views.UserProfileView.as_view(), name='user-profile'),
     path('users/', views.UserListView.as_view(), name='user-list'),
     path('users/<int:pk>/', views.UserDetailView.as_view(), name='user-detail'),
+]
+'''
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Authentication
+    path('register/', views.UserRegistrationView.as_view(), name='user-register'),
+    path('login/', views.login_view, name='user-login'),
+    
+    # Profile
+    path('profile/', views.UserProfileView.as_view(), name='user-profile'),
+    
+    # User management (admin only)
+    path('', views.UserListView.as_view(), name='user-list'),
+    path('<int:pk>/', views.UserDetailView.as_view(), name='user-detail'),
 ]
